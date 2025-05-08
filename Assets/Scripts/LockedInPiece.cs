66efc35ee7a1092039b2645f9cd75a8d57b3eb55
Joshua Martin <joshuam120901@gmail.com>
2025-05-08 19:13:36 +0100

    gameplay fix?

--- Assets/Scripts/LockedInPiece.cs
+++ Assets/Scripts/LockedInPiece.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class LockedInPiece : MonoBehaviour
 {
     public GridManager gridManager;
+    public PopCounter popCounter;
     public int pieceNo;
     public MainPiecePos mainPiecePos;
     public SecondPiecePos secondPiecePos;
@@ -13,11 +14,19 @@ public class LockedInPiece : MonoBehaviour
 
     private int thisPiecePosX;
     private int thisPiecePosY;
+
+    public bool markedToPop = false;
+
+    private int leftCheck;
+    private int rightCheck;
+    private int upCheck;
+    private int downCheck;
     
     // Start is called before the first frame update
     void Start()
     {
         gridManager = FindObjectOfType<GridManager>();
+        popCounter = FindObjectOfType<PopCounter>();
         if (pieceNo == 1)
         {
             thisPiecePosX = mainPiecePos.piecePosX;
@@ -28,6 +37,10 @@ public class LockedInPiece : MonoBehaviour
             thisPiecePosX = secondPiecePos.piecePosX;
             thisPiecePosY = secondPiecePos.piecePosY;
         }
+        leftCheck = thisPiecePosX - 1;
+        rightCheck = thisPiecePosX + 1;
+        upCheck = thisPiecePosY + 1;
+        downCheck = thisPiecePosY - 1;
 
     }
 
@@ -41,8 +54,67 @@ public class LockedInPiece : MonoBehaviour
                 transform.position += new Vector3(0, -1, 0);
                 gridManager.grid[thisPiecePosX, thisPiecePosY] = null;
                 thisPiecePosY -= 1;
-                gridManager.grid[thisPiecePosX, thisPiecePosY] = thisPiece; 
+                gridManager.grid[thisPiecePosX, thisPiecePosY] = thisPiece;
+                leftCheck = thisPiecePosX - 1;
+                rightCheck = thisPiecePosX + 1;
+                upCheck = thisPiecePosY + 1;
+                downCheck = thisPiecePosY - 1;
             }
         }
+        PopCheck();
+    }
+
+    void PopCheck()
+    {
+        if (leftCheck >= 0)
+        {
+            if (gridManager.grid[leftCheck, thisPiecePosY] != null)
+            {
+                if (gridManager.grid[leftCheck, thisPiecePosY].tag == thisPiece.tag)
+                {
+                    markedToPop = true;
+                    AddToPop();
+                }
+            }
+        }
+        if (rightCheck <= 7)
+        {
+            if (gridManager.grid[rightCheck, thisPiecePosY] != null)
+            {
+                if (gridManager.grid[rightCheck, thisPiecePosY].tag == thisPiece.tag)
+                {
+                    markedToPop = true;
+                    AddToPop();
+                }
+            }
+        }
+        if (upCheck <= 13)
+        {
+            if (gridManager.grid[thisPiecePosX, upCheck] != null)
+            {
+                if (gridManager.grid[thisPiecePosX, upCheck].tag == thisPiece.tag)
+                {
+                    markedToPop = true;
+                    AddToPop();
+                }
+            }
+        }
+        if (downCheck >= 0)
+        {
+            if (gridManager.grid[thisPiecePosX, downCheck] != null)
+            {
+                if (gridManager.grid[thisPiecePosX, downCheck].tag == thisPiece.tag)
+                {
+                    markedToPop = true;
+                    AddToPop();
+                }
+            }
+        }
+    }
+
+    void AddToPop()
+    {
+        popCounter.popCount += 1;
+        //popCounter.piecesToPop.Add(GameObject.this);
     }
 }
